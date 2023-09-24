dee50884fb2f144036f313269329cb7547cbcc47
Eavid <ganrn@vip.qq.com>
2023-09-24 20:22:34 +0800

    test: 测试配置调整为工作区

---
+++ packages/evlib/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineProject } from "vitest/config";
+import path from "node:path";
+
+const root = path.resolve(__dirname);
+export default defineProject({
+    test: {
+        alias: [
+            { find: /^evlib$/, replacement: path.resolve(root, "src/core/index.js") },
+            { find: /^evlib(?=\/[^\/]+$)/, replacement: path.resolve(root, "src") }, //只匹配 evlib/xxxx ; 不匹配 evlib/xxx/xxx
+        ],
+    },
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -8,10 +8,5 @@ export default defineConfig({
         coverage: {
             exclude: ["**/__mocks__"],
         },
-        alias: [
-            { find: /^evlib$/, replacement: path.resolve(root, "evlib/src/core/index.js") },
-            { find: /^evlib(?=\/[^\/]+$)/, replacement: path.resolve(root, "evlib", "src") }, //只匹配 evlib/xxxx ; 不匹配 evlib/xxx/xxx
-            { find: /^@eavid\/lib-node/, replacement: path.resolve(root, "lib-node/src") },
-        ],
     },
 });
