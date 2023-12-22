d3458ef7818f3d6e472f73538ce75c9b81f5d7f7
Eavid <ganrn@vip.qq.com>
2023-12-22 18:41:53 +0800

    chore: 更新 rollup 配置

--- packages/evlib/rollup.config.js
+++ packages/evlib/rollup.config.js
@@ -1,15 +1,10 @@
-import * as rollup from "@eavid/lib-dev/rollup"
-import * as rollupTool from "@eavid/lib-dev/rollup/tool";
+// @ts-check
+import { defineEvConfig, tools, } from "@eavid/lib-dev/rollup"
 import * as path from "node:path";
-
-/** @type {typeof import("@eavid/lib-dev/out/rollup/mod")} */
-const { defineEvConfig, defineConfig } = rollup
-/** @type {typeof import("@eavid/lib-dev/out/rollup/tool")} */
-const { getDirFiles } = rollupTool
 
 
 const dir = process.cwd();
-let files = await getDirFiles(path.resolve(dir, "src")) //获取src目录下的入口文件
+let files = await tools.getDirFiles(path.resolve(dir, "src")) //获取src目录下的入口文件
 files = files.filter((path) => path.endsWith(".ts")) //排除 src/tsconfig.json
 const corePath = path.resolve(dir, "src/core/index.ts")
 files.push(corePath)
@@ -20,7 +15,7 @@ export default defineEvConfig({
         dir: "dist",
         chunkFileNames: "internal/[name].js",
         minifyInternalExports: false,
-        sourcemap: true,
+        sourcemap: false,
         entryFileNames(info) {
             let add = ""
             if (info.facadeModuleId === corePath) add = "core/"
--- packages/lib-node/rollup.config.js
+++ packages/lib-node/rollup.config.js
@@ -1,25 +1,16 @@
-
-
-
-import * as rollup from "@eavid/lib-dev/rollup"
-import * as rollupTool from "@eavid/lib-dev/rollup/tool";
+// @ts-check
+import { defineEvConfig, tools } from "@eavid/lib-dev/rollup"
 import * as path from "node:path";
 
-/** @type {typeof import("@eavid/lib-dev/out/rollup/mod")} */
-const { defineEvConfig, defineConfig } = rollup
-/** @type {typeof import("@eavid/lib-dev/out/rollup/tool")} */
-const { getDirFiles } = rollupTool
-
-
 const dir = process.cwd();
-const files = await getDirFiles(path.resolve(dir, "src"))
+const files = await tools.getDirFiles(path.resolve(dir, "src"))
 export default defineEvConfig({
     input: files,
     output: {
         dir: "dist",
         chunkFileNames: "internal/[name].js",
         minifyInternalExports: false,
-        sourcemap: true,
+        sourcemap: false,
     },
 
     external: [/^evlib/],
