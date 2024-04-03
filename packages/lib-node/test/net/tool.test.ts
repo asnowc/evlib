3146813bb9dd8b861dc0f513f66ca871df34edc9
Eavid <ganrn@vip.qq.com>
2024-04-03 16:27:13 +0800

    refactor(lib-node): 删除无用文件

--- packages/lib-node/test/net/tool.test.ts
+++
@@ -1,5 +0,0 @@
-import { createConnectedTcp } from "../../src/net/tool.js";
-import { test, describe, expect, vi } from "vitest";
-import { Duplex, Readable } from "node:stream";
-
-describe.skip("createConnectedTcp", function () {}, 1000);
